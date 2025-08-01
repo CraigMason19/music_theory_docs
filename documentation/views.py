ae605717635bce25b0da9d8d2e2512687099719a
SadHappy <craigjm19@gmail.com>
2025-08-01 20:02:04 +0100

    Proof of concept, which python django view template

--- documentation/views.py
+++ documentation/views.py
@@ -1,8 +1,11 @@
 import inspect
 
 from django.shortcuts import render
+from django.utils.safestring import mark_safe
 
 import music_theory as mt
+
+ 
 
 def docs_view(request):
     functions = []
@@ -17,7 +20,44 @@ def docs_view(request):
 
 
 def notes_view(request):
+    attrs = [name for name, _ in inspect.getmembers(mt.Note)]
+    # Note.next
     attrs = [name for name, _ in inspect.getmembers(mt.Note, inspect.ismethod)]
+    # attrs = [name for name, _ in inspect.getmembers(mt.Note, inspect.ismethod) if not name.startswith("__")]
 
 
-    return render(request, "notes.html", {"attributes": attrs})
+    target_class = mt.Note  # Replace with your actual class
+
+    # Get methods defined *directly* in this class's file
+    # attrs = []
+    # for name, member in inspect.getmembers(target_class, inspect.isfunction):
+    #     # Check if the method is defined in the same source file as the class
+    #     if inspect.getsourcefile(member) == inspect.getsourcefile(target_class):
+    #         attrs.append(name)
+
+
+ 
+
+
+ 
+    # formatted = mark_safe(f"<pre>{mt.Note.__doc__}</pre>")
+    formatted = mark_safe(f"{mt.Note.__doc__}")
+
+
+    doc_strs = [
+        { "name": "Class: " + mt.Note.__name__, "doc_str": formatted },
+        { "name": "Method: " + mt.Note.from_index.__name__, "doc_str": mark_safe(f"{mt.Note.from_index.__doc__}") },
+        { "name": "Function: " + mt.notes.transpose.__name__, "doc_str": mark_safe(f"{mt.Note.transpose.__doc__}") },
+    ]
+
+    context = {
+        "module_name": mt.notes.__name__,
+        "class_doc": mark_safe(formatted),
+        "doc_strs": doc_strs,
+    }
+
+    return render(request, "notes.html", context)
+
+ 
+
+ 
