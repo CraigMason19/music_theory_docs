3f79450e9ac6d5b109c701ddbbca894f97fa47e8
SadHappy <craigjm19@gmail.com>
2025-09-05 18:17:50 +0100

    renamed urls to match function calls

--- documentationApp/urls.py
+++ documentationApp/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.documentation_view, name='documentation'),
-    path("examples/", views.examples_view, name="examples"),
-    path("tools/", views.tools_view, name="tools"),
-    path("<str:module_name>/", views.module_view, name="module_docs"),
+    path('', views.documentation_view, name='documentation_view'),
+    path("examples/", views.examples_view, name="examples_view"),
+    path("tools/", views.tools_view, name="tools_view"),
+    path("<str:module_name>/", views.module_view, name="module_view"),
 ]
