14d0a1784cfe64561fc90b6f5f235c14a5684c9f
SadHappy <craigjm19@gmail.com>
2025-09-06 11:07:04 +0100

    placeholder message integration

--- bake_app/views.py
+++ bake_app/views.py
@@ -1,7 +1,8 @@
+from django.contrib import messages
 from django.shortcuts import render
 
-
 def bake_view(request):
-    
+
+    messages.success(request, "Baked successfully.")
 
     return render(request, "bake.html")
