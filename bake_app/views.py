06575cb49b12a6ac9f4ad07f04dcd3aa90bf24a7
SadHappy <craigjm19@gmail.com>
2025-09-06 11:39:03 +0100

    Added bake button placeholder functionality

--- bake_app/views.py
+++ bake_app/views.py
@@ -1,8 +1,17 @@
 from django.contrib import messages
+from django.shortcuts import redirect
 from django.shortcuts import render
 
+def bake():
+    return True
+
 def bake_view(request):
-
-    messages.success(request, "Baked successfully.")
-
+    if request.method == "POST":
+        if bake():
+            messages.success(request, "Baked successfully.")
+        else:
+            messages.error(request, "Baked failed.")
+        
+        return redirect("bake_view")
+    
     return render(request, "bake.html")
