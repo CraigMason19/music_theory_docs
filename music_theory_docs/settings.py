26b85aa3da152791de8e68dc743bb8fc13c6c345
SadHappy <craigjm19@gmail.com>
2025-09-07 20:04:04 +0100

    Added custom settings

--- music_theory_docs/settings.py
+++ music_theory_docs/settings.py
@@ -15,6 +15,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Custom variables
+BAKE_MODE = False
+DEFAULT_PORT = "http://127.0.0.1:8000"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
