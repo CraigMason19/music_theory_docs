ba7dab9b8e6808f9f86a89a59f13785852ea8b94
SadHappy <craigjm19@gmail.com>
2025-09-19 15:36:32 +0100

    Added a js script to handle tool 2 onchange instead of in the html

---
+++ static/JS/tools.js
@@ -0,0 +1,15 @@
+
+
+// Tool 2
+const modesFromNoteForm = document.getElementById("modes-from-note-form");
+
+const toolTwoNoteInput = document.getElementById("tool-two-note-input");
+
+
+
+
+
+toolTwoNoteInput.addEventListener("change", () => {
+
+    modesFromNoteForm.submit()
+});
